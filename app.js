9544c7d7b54ff78e6a6bdf3eda6de3fc363cc429
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-07-01 19:08:31 +0300

    V 1.0

--- app.js
+++ app.js
@@ -2,14 +2,30 @@
 var togglePlayer = () => {
     if(currentPlayer === 'X'){
         currentPlayer = 'O';
+        setCurrentPlayer(currentPlayer);
     }else{
         currentPlayer = 'X';
+        setCurrentPlayer(currentPlayer);
     }
 };
 
+var setCurrentPlayer = (Player) => {
+    currentPlayerDiv.innerHTML = 'Current Player \n <h1 class="h1">'+Player+'</h1>';
+}
+
+var setWinnerPlayer = (Player) => {
+    winningPlayerDiv.innerHTML = 'Winner Player\n<h1 class="winner">'+Player+'</h1>';
+}
+
+var setDraw = () => {
+    winningPlayerDiv.innerHTML = '<h1 class="draw">Draw</h1>';
+} 
+
 var init = () => {
     currentPlayer = 'X'; 
-    winningPlayerDiv.innerText = '';
+    winnerPlayer = undefined;
+    setCurrentPlayer(currentPlayer);
+    winningPlayerDiv.innerHTML = '';
     //console.log('init called'); 
     firstRow.childNodes.forEach(e => {
         e.textContent = '';
@@ -27,65 +43,89 @@ var init = () => {
 };
 
 var checkForWinByRow = (obj) => {
-    if(((obj[1] === obj[2]) && (obj[2] === obj[3]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[1];
-        win = true;
-        return;
-    } else if (((obj[4] === obj[5]) && (obj[5] === obj[6]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[4];
-        win = true;
-        return;
-    }else if (((obj[7] === obj[8]) && (obj[8] === obj[9]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[7];
-        win = true;
-        return;
-    }
-};
-var checkForWinByColumn = (obj) => {
-    if(((obj[1] === obj[4]) && (obj[4] === obj[7]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[1];
-        win = true;
-        return;
-    } else if (((obj[2] === obj[5]) && (obj[2] === obj[8]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[2];
-        win = true;
-        return;
-    }else if (((obj[3] === obj[6]) && (obj[3] === obj[9]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[3];
-        win = true;
-        return;
-    }
-};
-var checkForWinDiagonally = (obj) => {
-    if(((obj[1] === obj[5]) && (obj[1] === obj[9]))){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[1];
-        win = true;
-        return;
-    } else if ((obj[3] === obj[5]) && (obj[3] === obj[7])){
-        winningPlayerDiv.innerText = 'Winning Player is : ' + obj[3];
-        win = true;
-        return;
+    if((obj[1] === obj[2]) && (obj[2] === obj[3])){
+        setWinnerPlayer(obj[1]);
+        winnerPlayer = obj[1];
+        return true;
+    } else if ((obj[4] === obj[5]) && (obj[5] === obj[6])){
+        setWinnerPlayer(obj[4]); 
+        winnerPlayer = obj[4];
+        return true;
+    }else if ((obj[7] === obj[8]) && (obj[8] === obj[9])){
+        setWinnerPlayer(obj[7]); 
+        winnerPlayer = obj[7];
+        return true;
     }
 };
 
-var checkDraw = (obj) => {
-    if(obj[1] && obj[2] && obj[3] && obj[4] 
-        && obj[5] && obj[6] && obj[7] & obj[8] && obj[9])
-    {
-        winningPlayerDiv.innerText = 'Draw';
-        return;    
+var checkForWinByColumn = (obj) => {
+    if((obj[1] === obj[4]) && (obj[4] === obj[7])){         
+        setWinnerPlayer(obj[1]);
+        winnerPlayer = obj[1];
+        return true;
+    } else if ((obj[2] === obj[5]) && (obj[2] === obj[8])){
+        setWinnerPlayer(obj[2]);
+        winnerPlayer = obj[2];
+        return true;
+    }else if ((obj[3] === obj[6]) && (obj[3] === obj[9])){
+        setWinnerPlayer(obj[3]); 
+        winnerPlayer = obj[3];
+        return true;
     }
-}
+};
+
+var checkForWinDiagonally = (obj) => {
+    if((obj[1] === obj[5]) && (obj[1] === obj[9])){
+        setWinnerPlayer(obj[1]);
+        winnerPlayer = obj[1];
+        return true;
+    } else if ((obj[3] === obj[5]) && (obj[3] === obj[7])){
+        setWinnerPlayer(obj[3]); 
+        winnerPlayer = obj[3];
+        return true;
+    }
+};
 
 var checkForWin = (obj) => {
     //check winning by row
-    checkForWinByRow(obj);
-    //check winning by column
-    checkForWinByColumn(obj);
-    // check winning Diagonally
-    checkForWinDiagonally(obj);
-    // check for draw
-    checkDraw(obj);
+    if(checkForWinByRow(obj) || checkForWinByColumn(obj) 
+    || checkForWinDiagonally(obj)){
+        return true;
+    }
+}
+
+var checkDraw = (obj) => {
+    if(checkAllBoxes(obj)){
+        if(checkForWinByRow(obj) || checkForWinByColumn(obj) 
+        || checkForWinDiagonally(obj)){
+            return false;
+        }
+        return true;        
+    }    
+}
+
+var checkAllBoxes = (obj) => {
+    for(var key in obj){
+        if(obj[key] === ''){
+            return false;
+        }
+    }
+    return true;
+}
+
+var addToBoard = (e) => {
+    if(winnerPlayer === undefined || winnerPlayer === ''){
+        if(e.textContent === ''){
+            e.textContent = currentPlayer;
+            gameBoard[e.id] = currentPlayer;
+            togglePlayer();        
+            setCurrentPlayer(currentPlayer);        
+        } 
+        checkForWin(gameBoard); 
+        if(checkDraw(gameBoard)){
+            setDraw();
+        }       
+    }    
 }
 //--------------- END OF HELPER FUNCTIONS -------------------
 
@@ -96,43 +136,24 @@ var currentPlayerDiv = document.getElementById('currentPlayer');
 var winningPlayerDiv = document.getElementById('WinningPlayer');
 var resetBtn = document.getElementById('reset');
 var gameBoard = {
-    1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:""
+    1:'', 2:'', 3:'', 4:'', 5:'', 6:'', 7:'', 8:'', 9:''
 };
 var currentPlayer = 'X';
-var win = false;
+var winnerPlayer;
 init();
+
 resetBtn.onclick = () => {
-    init();
+  init();
 };
 
-currentPlayerDiv.innerText = 'Current Player: '+currentPlayer;
-
 firstRow.childNodes.forEach(e => e.addEventListener('click',() => {    
-    if(e.textContent === '' && win === true){
-        e.textContent = currentPlayer;
-        gameBoard[e.id] = currentPlayer;
-        togglePlayer();        
-        currentPlayerDiv.innerText = 'Current Player: '+currentPlayer;
-    }
-    checkForWin(gameBoard);
+  addToBoard(e);
 }));
-
+    
 secondRow.childNodes.forEach(e => e.addEventListener('click',() => {    
-    if(e.textContent === '' && win === true){
-        e.textContent = currentPlayer;
-        gameBoard[e.id] = currentPlayer;
-        togglePlayer();
-        currentPlayerDiv.innerText = 'Current Player: '+currentPlayer;
-    }
-    checkForWin(gameBoard);
+  addToBoard(e);
 }));
-
+    
 thirdRow.childNodes.forEach(e => e.addEventListener('click',() => {    
-    if(e.textContent === '' && win === true){
-        e.textContent = currentPlayer;
-        gameBoard[e.id] = currentPlayer;
-        togglePlayer();
-        currentPlayerDiv.innerText = 'Current Player: '+currentPlayer;
-    }
-    checkForWin(gameBoard);
+  addToBoard(e);
 }));
